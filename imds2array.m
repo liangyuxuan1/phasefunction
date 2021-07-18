9100db3f5f07cb3221ea9fb6b419cc2bb4a6dd5d
liangyuxuan1 <872277196@qq.com>
2021-07-18 18:49:14 +0800

    initial commit

---
+++ imds2array.m
@@ -0,0 +1,24 @@
+function [X, T] = imds2array(imds)
+% imds2array   Convert an image datastore into a 4-D array
+%
+%   X       - Input data as an H-by-W-by-C-by-N array, where H is the
+%             height and W is the width of the images, C is the number of
+%             channels, and N is the number of images.
+%   T       - Categorical vector containing the labels for each
+%             observation.
+
+% Copyright 2016 The MathWorks, Inc.
+
+% modified by jimin to read mat
+
+imagesCellArray = imds.readall();
+numImages = numel( imagesCellArray );
+[h, w, c] = size( imagesCellArray{1}.rawData );
+
+X = zeros( h, w, c, numImages );
+for i=1:numImages
+    X(:,:,:,i) = imagesCellArray{i}.rawData ;
+end
+
+T = imds.Labels;
+end
