da04dfa669633bd564bab4f5ef192accb771023c
liangyuxuan1 <872277196@qq.com>
2021-08-14 15:48:54 +0800

    add data selection

--- Step2_ExcludeExtremes.m
+++ Step2_ExcludeExtremes.m
@@ -4,25 +4,34 @@ clc;
 srcDataPath = 'imageCW_v3';     % path to the datalist
 dstDataPath = 'imageCW_v3';     % path to store the reduced datalist
 
-changeDataList(srcDataPath, 'trainDataCW_v3_image.csv', 'trainDataCW_v3_ExcludeExtremes.csv');
-changeDataList(srcDataPath, 'testDataCW_v3_image.csv',  'valDataCW_v3_ExcludeExtremes.csv');
+changeDataList(srcDataPath, 'trainDataCW_v3_image.csv', 'trainDataCW_v3_ExcludeExtremes_small.csv', 40);
+changeDataList(srcDataPath, 'testDataCW_v3_image.csv',  'valDataCW_v3_ExcludeExtremes_small.csv', 90);
 
-function changeDataList(srcDataPath, srcDataListFileName, dstDataListFileName)
+function changeDataList(srcDataPath, srcDataListFileName, dstDataListFileName, imgNumThreshold)
     T = readtable(fullfile(srcDataPath, srcDataListFileName));
     
     num_pixels = table2array(T(:, 5));
     idx = find(num_pixels < 100);
     
-    filename = cell2mat(table2array(T(idx, 1)));
-    sepPos = strfind(filename(1,:), filesep);
-    subDirs = filename(:, 1:sepPos-1);
+    filenames = cell2mat(table2array(T(idx, 1)));
+    sepPos = strfind(filenames(1,:), filesep);
+    subDirs = filenames(:, 1:sepPos-1);
     [dirNames, ia, ic] = unique(subDirs, 'rows');
     for i=1:length(dirNames)
         dirNum = length(find(ic == i));
         fprintf('%s: %d\n', dirNames(i,:), dirNum);
     end
 
-    T(idx,:) = [];
+    T(idx,:) = [];  % remove extreme images
+
+    filenames = cell2mat(table2array(T(:, 1)));
+    dotPos = strfind(filenames(1,:), '.');
+    imgNums = filenames(:, dotPos(1)-4:dotPos(1)-1);
+    imgNums = str2num(imgNums);
+    idx = find(imgNums > imgNumThreshold);
+    
+    T(idx,:) = [];  % keep only part of images
+    
     writetable(T, fullfile(srcDataPath, dstDataListFileName));
 end
 
